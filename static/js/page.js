91af5b72a4aa52f1a97ba67f18b5a2695aa659e0
Cristian Traistaru <traistar@us.ibm.omc>
2015-07-14 11:15:25 -0500

    Added navbar buttons w/ same page nav support

--- static/js/page.js
+++ static/js/page.js
@@ -3,14 +3,17 @@ var App = App || {};
 
 App.Page = ( function() {
 
-  var currentPage = null;
+  var currentPage = null, currentPageNav = null;
   var isNavigating = false;
 
   function finishNavigateTo( newPage , loadCallback )
   {
     currentPage = $( newPage );
+    currentPageNav = $( newPage + "Nav" );
+
     currentPage.fadeIn( 200 , function() {
       isNavigating = false;
+      currentPageNav.show();
     } );
 
     if( loadCallback && typeof loadCallback == "function" )
@@ -23,6 +26,11 @@ App.Page = ( function() {
   {
     if( isNavigating === true ) return;
     isNavigating = true;
+
+    if( currentPageNav !== null )
+    {
+      currentPageNav.hide();
+    }
 
     if( currentPage === null )
     {
