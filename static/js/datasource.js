db7a3016059edae89a6e06d18d2e4cd62d6cffdc
Cristian Traistaru <traistar@us.ibm.omc>
2015-08-07 18:40:56 -0500

    Updated client datasource to use new format

--- static/js/datasource.js
+++ static/js/datasource.js
@@ -2,6 +2,7 @@
 var App = App || {};
 
 App.Datasource = function( id ) {
+  this.config = App.Datasource.datasources[ id ];
   this.id = id;
   this.chartCount = 0;
   this.charts = {};
@@ -35,23 +36,43 @@ App.Datasource.prototype.isEmpty = function() {
   return this.chartCount === 0;
 };
 
+App.Datasource.prototype.convertData = function( data ) {
+  if( this.config.tstampField )
+  {
+    data.tstamp = data[ this.config.tstampField ];
+    delete data[ this.config.tstampField ];
+  }
+
+  if( this.config.dataField )
+  {
+    data.data = data[ this.config.dataField ];
+    delete data[ this.config.dataField ];
+  }
+
+  return data;
+};
+
 App.Datasource.prototype.pushData = function( data ) {
+  data = this.convertData( data );
+
   for( var id in this.charts )
   {
     this.charts[id].chart.pushData( this.charts[id].index , data );
   }
 };
 
-// TODO: Flip start and end properly
-App.Datasource.prototype.requestHistoryData = function( end , start ) {
-  if( this.end !== null )
-  {
-    if( end >= this.end ) return;
-    if( start > this.end ) start = this.end;
-  }
+App.Datasource.prototype.requestHistoryData = function( index , start , end , callback ) {
+  var self = this;
 
-  this.end = end;
-  App.Net.requestHistoryData( this.id , start , end );
+  $.getJSON( "api/datasources/history?id=" + this.id + "&start=" + start + "&end=" + end ).done( function( data ) {
+    if( self.config.tstampField || self.config.dataField )
+    {
+      for( var i = 0; i < data.length; i++ )
+        data[i] = self.convertData( data[i] );
+    }
+
+    if( typeof callback === "function" ) callback( index , data );
+  } );
 };
 
 App.Datasource.getDatasources = function() {
