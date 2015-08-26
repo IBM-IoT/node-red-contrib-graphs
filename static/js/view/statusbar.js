f3e05b99e2edcbfb7f678a984ca0c3d5a222dde0
Cristian Traistaru <traistar@us.ibm.com>
2015-08-26 13:13:03 -0500

    Added status bar

---
+++ static/js/view/statusbar.js
@@ -0,0 +1,31 @@
+
+var App = App || {};
+App.View = App.View || {};
+
+App.View.Status = ( function() {
+
+  function set( msg )
+  {
+    var time = ( new Date() ).toLocaleTimeString();
+    $( "#statusBarMessage" ).text( "[" + time + "] " + msg );
+  }
+
+  function clear()
+  {
+    $( "#statusBarMessage" ).text( "" );
+  }
+
+  function setConnected( connected )
+  {
+    var $target = $( ".statusBarConnect" );
+    if( connected ) $target.addClass( "connected" );
+    else $target.removeClass( "connected" );
+  }
+
+  return {
+    set : set,
+    clear : clear,
+    setConnected : setConnected
+  };
+
+} )();
