923528f936529452caf2b4f788672386497b030b
Cristian Traistaru <traistar@us.ibm.com>
2015-09-15 15:34:54 -0500

    Added index.html mustache template along with...
    ...support for node-red's httpNodeRoot configuration. The <base> tag is
    now properly changed to reflect httpNodeRoot.

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 
+var fs = require( "fs" );
 var express = require( "express" );
+var Mustache = require( "mustache" );
+
 var plugins = require( "./plugins" );
 var users = require( "./users" );
 var datasources = require( "./datasources" );
 
 function init( RED )
 {
+  fs.readFile( __dirname + "/template/index.mst" , function( err , data ) {
+    if( err ) throw err;
+
+    var rendered = Mustache.render( data.toString() , { baseUrl : RED.settings.get( "httpNodeRoot" ) } );
+    fs.writeFile( __dirname + "/static/index.html" , rendered , function( err ) {
+      if( err ) throw err;
+    } );
+  } );
+
   RED.log.info( "Dashboard up and running" );
   RED.httpNode.use( "/dash" , express.static( __dirname + "/static" ) );
 
